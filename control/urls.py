5334edc537e0fd7560d7c8fd3683ed7a30aece6c
James Melvin <j2melvin@gmail.com>
2016-07-12 21:07:16 -0600

    Version 1 complete

--- control/urls.py
+++ control/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^live_feed$', views.live_feed, name='live_feed'),
 ]
--- control/views.py
+++ control/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
+from datetime import datetime
+import os
+import pytz
+import RPi.GPIO as GPIO
+import time
+
+CAMERA_FILE = "/home/pi/camera/current.jpg"
 
 
+@method_decorator(csrf_exempt)
 def index(request):
-    context = {'first_name': 'John',
-               'last_name': 'Doe'}
+    context = {}
+    if request.method == 'POST':
+        if request.POST.get('trigger', '0') == '1':
+            # Trigger garage door opener
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setwarnings(False)
+            GPIO.setup(7, GPIO.OUT)
+            GPIO.output(7, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(7, GPIO.LOW)
+
+        return render(request, 'control/success.html', context)
+
     return render(request, 'control/index.html', context)
+
+def live_feed(request):
+    mountain = pytz.timezone('US/Mountain')
+    try:
+        last_mod = datetime.utcfromtimestamp(os.path.getmtime(CAMERA_FILE)).replace(tzinfo=pytz.utc)
+        context = {'last_update': str(last_mod.astimezone(mountain).strftime("%Y-%m-%d %I:%M:%S %p"))}
+    except:
+        context = {'last_update': "Not Available"}
+
+    return render(request, 'control/live_feed.html', context)
+
