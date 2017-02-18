193484078ba5b8a66ca147bdbef28025628a70ce
James Melvin <j2melvin@gmail.com>
2017-02-18 16:04:47 -0700

    Add REST API

--- ibrox/urls.py
+++ ibrox/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework import routers
+from control import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^control/', include('control.urls')),
     url(r'^admin/', admin.site.urls),
 ]
